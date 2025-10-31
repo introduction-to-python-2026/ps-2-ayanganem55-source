360b64329b32cd0a57344ad8823e71cdc8e730a4
ayanganem55-source <ayanganem55@gmail.com>
2025-10-31 17:19:17 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -6,7 +6,7 @@ def find_max_number(num1, num2, num3):
     else:
         return num3
 
-def find_mean(num1, num2, num3)
+def find_mean(num1, num2, num3):
   find_mean = (num1 + num2 + num3) /3
     return find_mean
 
